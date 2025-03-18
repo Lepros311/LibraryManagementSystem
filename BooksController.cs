983c3ff7f4cd0e664a6901b429e9a95f5e93c482
Lepros311 <andy.strickland@gmail.com>
2025-03-17 21:07:06 -0400

    Update code

--- BooksController.cs
+++ BooksController.cs
@@ -1,68 +1,81 @@
-﻿using Spectre.Console;
+﻿using LibraryManagementSystem.Models;
+using Spectre.Console;
 
 namespace LibraryManagementSystem;
 
 internal class BooksController
 {
-    private List<string> books = new()
-    {
-
-    "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye", "The Hobbit", "Moby-Dick", "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", "Animal Farm", "Brave New World", "The Chronicles of Narnia", "The Diary of a Young Girl", "The Alchemist", "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
-    };
-
-
-
     internal void ViewBooks()
     {
-        /* Spectre's MarkupLine method is useful for styling strings. We'll use it as a standard to print messages to the console. */
-        AnsiConsole.MarkupLine("[yellow]List of Books:[/]");
-        // Printing each book to the console with a loop
-        foreach (var book in MockDatabase.Books)
+        var table = new Table();
+        table.Border(TableBorder.Rounded);
+
+        table.AddColumn("[yellow]ID[/]");
+        table.AddColumn("[yellow]Title[/]");
+        table.AddColumn("[yellow]Author[/]");
+        table.AddColumn("[yellow]Category[/]");
+        table.AddColumn("[yellow]Location[/]");
+        table.AddColumn("[yellow]Pages[/]");
+
+        // Filtering only items of the book type
+        var books = MockDatabase.Books.OfType<Book>();
+
+        foreach (var book in books)
         {
-            AnsiConsole.MarkupLine($"- [cyan]{book.Name}[/] - [yellow]{book.Pages} pages[/]");
+            table.AddRow(
+                book.Id.ToString(),
+                $"[cyan]{book.Name}[/]",
+                $"[cyan]{book.Author}[/]",
+                $"[green]{book.Category}[/]",
+                $"[blue]{book.Location}[/]",
+                book.Pages.ToString()
+                );
         }
-        /* Having the user press a key before continuing, or the menu wouldn't be visualisable. */
-        AnsiConsole.MarkupLine("Press any key to continue.");
+
+        AnsiConsole.Write(table);
+        AnsiConsole.MarkupLine("Press Any Key to Continue.");
         Console.ReadKey();
     }
 
     internal void AddBook()
     {
-        /* Spectre's Ask<> method allows us to prompt a message to grab the user's input. We can pass the type we expect as an answer for validation. We're storing the answer in the 'title' variable.*/
         var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add:");
+        var author = AnsiConsole.Ask<string>("Enter the [green]author[/] of the book:");
+        var category = AnsiConsole.Ask<string>("Enter the [green]category[/] of the book:");
+        var location = AnsiConsole.Ask<string>("Enter the [green]location[/] of the book:");
         var pages = AnsiConsole.Ask<int>("Enter the [green]number of pages[/] in the book:");
-        // Checking if the book already exists to avoid duplication.
-        if (MockDatabase.Books.Exists(b => b.Name.Equals(title, StringComparison.OrdinalIgnoreCase)))
+
+        if (MockDatabase.Books.OfType<Book>().Any(b => b.Name.Equals(title, StringComparison.OrdinalIgnoreCase)))
         {
             AnsiConsole.MarkupLine("[red]This book already exists.[/]");
         }
         else
         {
-            // If book doesn't exist, add to the list of books.
-            var newBook = new Book(title, pages);
+            var newBook = new Book(MockDatabase.Books.Count + 1, title, author, category, location, pages);
             MockDatabase.Books.Add(newBook);
             AnsiConsole.MarkupLine("[green]Book added successfully![/]");
         }
-        AnsiConsole.MarkupLine("Press any key to continue.");
+
+        AnsiConsole.MarkupLine("Press Any Key to Continue.");
         Console.ReadKey();
     }
 
     internal void DeleteBook()
     {
-        // Checking if there are any books to delete and letting the user know.
-        if (MockDatabase.Books.Count == 0)
+        var books = MockDatabase.Books.OfType<Book>().ToList();
+
+        if (books.Count == 0)
         {
             AnsiConsole.MarkupLine("[red]No books available to delete.[/]");
             Console.ReadKey();
             return;
         }
-        /* Showing a list of books and letting the user choose with arrows using SelectionPrompt, similarly to what we do with the menu */
+
         var bookToDelete = AnsiConsole.Prompt(
             new SelectionPrompt<Book>()
-            .Title("Select a [red]book[/] to delete:")
-            .UseConverter(b => $"{b.Name}")
-            .AddChoices(MockDatabase.Books));
-        /* Using the Remove method to delete a book. This method returns a boolean that we can use to present a message in case of success or failure. */
+                .Title("Select a [red]book[/] to delete:")
+                .AddChoices(books));
+
         if (MockDatabase.Books.Remove(bookToDelete))
         {
             AnsiConsole.MarkupLine("[red]Book deleted successfully![/]");
@@ -71,7 +84,8 @@ internal class BooksController
         {
             AnsiConsole.MarkupLine("[red]Book not found.[/]");
         }
-        AnsiConsole.MarkupLine("Press any key to continue.");
+
+        AnsiConsole.MarkupLine("Press Any Key to Continue.");
         Console.ReadKey();
     }
 }
